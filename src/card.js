665a59955b1fe6b8d115b99ca72e2182a3b69bdd
Julian Williams <julian.th.williams@gmail.com>
2021-10-11 19:01:02 -0700

    Completed funtionality but not style

--- src/card.js
+++ src/card.js
@@ -1,7 +1,11 @@
 const generateMembers = function (employee) {
   return `<div>
   <h2>${employee.name}</h2>
-  <h3>${employee.id}</h3>
+  <h1>${employee.id}</h1>
+  <h1>${employee.email}</h1>
+  <h2>${employee.role}</h2>
+  <h3 style="margin-bottom:${employee.office ? .5 : 0}">${employee.office ? employee.office : ""}</h3>
+  <h3 style="margin-bottom:${employee.github ? .5 : 0}">${employee.github ? employee.github : ""}</h3>
   <h3 style="margin-bottom:${employee.school ? .5 : 0}">${employee.school ? employee.school : ""}</h3>
   </div>`;
 };
